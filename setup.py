be9e174b9b934bd0b1573c2ebbcaa3f370a04e87
vineoy <113031024+vineoy@users.noreply.github.com>
2024-11-23 19:21:36 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApp',
+    version= '0.0.1',
+    author= 'Vinayak Singh',
+    author_email= 'probablytp@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
